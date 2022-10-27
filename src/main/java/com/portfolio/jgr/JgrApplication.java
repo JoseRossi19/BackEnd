a599ecf5ba2239d5b2f4e2d57caf7bfa2bd7bfd2
JoseRossi19 <jose.rossigarcia@gmail.com>
2022-10-27 20:22:50 -0300

    primer commit

---
+++ src/main/java/com/portfolio/jgr/JgrApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/jgr/JgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
