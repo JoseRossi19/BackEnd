45a3904ccfc79719a144021f3c42530dce2820e1
JoseRossi19 <jose.rossigarcia@gmail.com>
2022-11-02 23:53:32 -0300

    asd

--- src/main/java/com/portfolio/jgr/Security/MainSecurity.java
+++ src/main/java/com/portfolio/jgr/Security/MainSecurity.java
@@ -7,6 +7,8 @@ package com.portfolio.jgr.Security;
 import com.portfolio.jgr.Security.Service.UserDetailsImpl;
 import com.portfolio.jgr.Security.jwt.JwtEntryPoint;
 import com.portfolio.jgr.Security.jwt.JwtTokenFilter;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +22,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -42,16 +45,29 @@ public class MainSecurity extends WebSecurityConfigurerAdapter{
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .authorizeRequests()
-                .antMatchers("**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
+        List<String> list1 = Arrays.asList(new String[]{"Authorization", "Cache-Control", "Content-Type"});
+        List<String> list2 = Arrays.asList(new String[]{https://backendok.herokuapp.com});
+        List<String> list3 = Arrays.asList(new String[]{"GET", "POST", "PUT", "DELETE", "OPTIONS"});
+        List<String> list4 = Arrays.asList(new String[]{"Authorization"});
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(list1);
+        corsConfiguration.setAllowedOrigins(list2);
+        corsConfiguration.setAllowedMethods(list3);
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(list4);
+
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("**").permitAll();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().configurationSource(request -> corsConfiguration);
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
     }
+
 
     @Override
     protected AuthenticationManager authenticationManager() throws Exception {
