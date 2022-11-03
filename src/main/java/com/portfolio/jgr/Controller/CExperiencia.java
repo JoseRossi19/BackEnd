3ae83e3d9fe527154e62c99f84b1bb4b5bb10de6
JoseRossi19 <jose.rossigarcia@gmail.com>
2022-11-03 14:30:39 -0300

    as

--- src/main/java/com/portfolio/jgr/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/jgr/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "**")
+@CrossOrigin(origins = "/**")
 
 public class CExperiencia {
 
